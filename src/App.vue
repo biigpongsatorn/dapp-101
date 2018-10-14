e76942895b3028f5b49e105852ae8b3bdea4c98d
biigpongsatorn <biig_pongsatorn@hotmail.com>
2018-10-14 22:48:12 +0700

    done

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,127 @@
 <template>
   <div id="app">
-    <HelloWorld :msg="fullName"/>
-    <button @click="changeText">Click</button>
+    <div v-if="isReady">
+      <!-- If login Metamask -->
+      <div v-if="isTxStatus && hash">
+        <!-- Transaction status -->
+        <h1 :class="{
+          'cl-or': isTxStatus === 'Pending',
+          'cl-green': isTxStatus === 'Success'
+        }">
+          Transaction status: {{ isTxStatus }}
+        </h1>
+      </div>
+      <h1>
+        <!-- Name from Blockchain -->
+        Your name in Blockchain is : {{ myname }}
+      </h1>
+      <div>
+        <!-- input bla bla bla -->
+        <input type="text" v-model="nameInput" style="margin-right: 10px;">
+        <button @click="setMyname">Set name</button>
+      </div>
+      <br>
+      <div v-if="hash">
+        <!-- When transaction hashed will show this. -->
+        <b>
+          hash:
+          <a :href="`https://kovan.etherscan.io/tx/${hash}`" target="_blank">
+            {{ hash }}
+          </a>
+        </b>
+      </div>
+    </div>
+    <div v-else>
+      <!-- If not login metamask will it show this -->
+      <h1>Please login your fucking Metamask 🙂</h1>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   data () {
     return {
-      test: 'Hello',
-      firstName: 'Narong',
-      lastName: 'ABC'
+      // Smart contract address
+      simpleContractAddr: '0xeb4de03811769832344b03cd994be093106bf2e2',
+      // Smart contract ABI
+      simpleAbi: [
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "getMyName",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "name",
+              "type": "string"
+            }
+          ],
+          "name": "setMyName",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ],
+      contract: null,
+      myWalletAddress: [],
+      myname: '',
+      nameInput: '',
+      hash: '',
+      isTxStatus: '',
+      isReady: true
     }
   },
   methods: {
-    changeText () {
-      this.lastName = 'World'
+    setMyname () {
+      // function set name
+      this.contract.methods.setMyName(this.nameInput).send({
+        from: this.myWalletAddress[0]
+      }).on('transactionHash', (hash) => {
+        // when transaction hashed will call this event.
+        this.nameInput = ''
+        this.hash = hash
+        this.isTxStatus = 'Pending'
+      }).on('confirmation', (confirmationNumber) => {
+        // When transaction confirmed will call this event.
+        if (confirmationNumber === 1) {
+          this.isTxStatus = 'Success'
+          this.getMyname()
+        }
+      })
+    },
+    async getMyname () {
+      // Get your name in Blockchain.
+      this.myname = await this.contract.methods.getMyName().call()
     }
   },
-  computed: {
-    fullName () {
-      return this.firstName + ' ' + this.lastName
+  async mounted () {
+    // Connect smart contract
+    this.contract = await new this.$web3.eth.Contract(this.simpleAbi, this.simpleContractAddr)
+    // Get wallet address from Metamask
+    this.myWalletAddress = await this.$web3.eth.getAccounts()
+    // Is metamask login ?
+    if (!this.myWalletAddress.length) {
+      // Not login
+      this.isReady = false
+      alert('Please login you funcking Metamask :)')
+    } else {
+      // Loged in will call get name
+      this.getMyname()
     }
-  },
-  watch: {
-    fullName (newValue, oldValue) {
-      console.log(newValue, oldValue)
-    }
-  },
-  created () {
-    console.log('created.')
-    const dom = document.getElementById('app')
-    console.log(dom)
-  },
-  mounted () {
-    console.log('mounted.')
-    const dom = document.getElementById('app')
-    console.log(dom)
-  },
-  updated () {
-    console.log('updated.')
-    const dom = document.getElementById('app')
-    console.log(dom)
-  },
-  components: {
-    HelloWorld
   }
 }
 </script>
@@ -61,5 +134,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.cl-green {
+  color: green;
+}
+.cl-or {
+  color: orange;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Web3 from 'web3'
 
+// Initial web3.js
+let web3 = {}
+if (typeof window.web3 !== 'undefined') {
+  web3 = new Web3(window.web3.currentProvider)
+} else {
+  const httpProvider = new Web3.providers.HttpProvider('https://mainnet.infura.io/iT7hWgNoY7XT2BXIVAhq')
+  web3 = new Web3(httpProvider)
+}
+
+Vue.prototype.$web3 = web3
 Vue.config.productionTip = false
 
 new Vue({
